c3b1fa7d9ce47145c3b54cf8c11ecfe91e8996c6
Piotr Przybyl <przybylpio@gmail.com>
2021-05-12 22:02:25 +0200

    javaScript code amended

--- Script/script.js
+++ Script/script.js
@@ -1,38 +1,47 @@
 {
+    const formElement = document.querySelector(".js-form");
     const haveAmount = document.querySelector(".js-haveAmount");
     const giveAmount = document.querySelector(".js-giveAmount");
-    const formElement = document.querySelector(".js-form");
     const bidCurrency = document.querySelector(".js-bidCurrency");
     const askCurrency = document.querySelector(".js-askCurrency");
 
-    formElement.addEventListener("submit", (event) => {
-        event.preventDefault();
+    const calculateResult = () => {
+        const Bid = bidCurrency.value;
+        const Ask = askCurrency.value;
+        const Amount = +haveAmount.value;
+        const BidEURPLN = 4.55;
+        const AskEURPLN = 4.57;
+        const BidUSDPLN = 3.76;
+        const AskUSDPLN = 3.78;
+        const BidCHFPLN = 4.12;
+        const AskCHFPLN = 4.14;
 
-        switch (bidCurrency.value + askCurrency.value) {
+        switch (Bid + Ask) {
             case ("PLN" + "EUR"):
-                giveAmount.value = (haveAmount.value / 4.57).toFixed(0);
+                giveAmount.value = (Amount / AskEURPLN).toFixed(2);
                 break;
             case "EUR" + "PLN":
-                giveAmount.value = (haveAmount.value * 4.55).toFixed(0);
+                giveAmount.value = (Amount * BidEURPLN).toFixed(2);
                 break;
             case "PLN" + "USD":
-                giveAmount.value = (haveAmount.value / 3.78).toFixed(0);
+                giveAmount.value = (Amount / AskUSDPLN).toFixed(2);
                 break;
             case ("USD" + "PLN"):
-                giveAmount.value = (haveAmount.value * 3.76).toFixed(0);
+                giveAmount.value = (Amount * BidUSDPLN).toFixed(2);
                 break;
             case "PLN" + "CHF":
-                giveAmount.value = (haveAmount.value / 4.14).toFixed(0);
+                giveAmount.value = (Amount / AskCHFPLN).toFixed(2);
                 break;
             case "CHF" + "PLN":
-                giveAmount.value = (haveAmount.value * 4.12).toFixed(0);
+                giveAmount.value = (Amount * BidCHFPLN).toFixed(2);
                 break;
             default:
                 giveAmount.value = "wrong currency pair";
-
         }
+    }
+    formElement.addEventListener("submit", (event) => {
+        event.preventDefault();
 
-
-
+        calculateResult();
     });
 }
